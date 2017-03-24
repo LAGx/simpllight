74440d40602bddb673af4d264463bd1b798cafb6
LAG_x <lag.x@yandex.com>
2017-03-24 22:39:31 +0200

    Fix pos & id of screen log
    detele of pos of log.
    pos is id.

--- main.cpp
+++ main.cpp
@@ -20,10 +20,10 @@ int main() {
 	Log::log("____________LOG SESSION START__________", true);
 
 	ScreenLog screenLog;
-	screenLog.setNewLog("FPS", 1, 0);
-	screenLog.setNewLog("collision h1", 2, 1);
-	screenLog.setNewLog("collision h2", 3, 2);
-	screenLog.setNewLog("Mouse: ", 4, 3);
+	screenLog.setNewLog("FPS", 0);
+	screenLog.setNewLog("collision h1", 1);
+	screenLog.setNewLog("collision h2", 2);
+	screenLog.setNewLog("Mouse: ", 3);
 	spl::Window window;
 
 
@@ -70,7 +70,7 @@ int main() {
 		//window.view.setCenter(sf::Vector2f(player.body_ph->GetPosition().x*SCALE_BOX, player.body_ph->GetPosition().y*SCALE_BOX));
 		/////////////// FPS //////////////////
 		if (i > 10) {
-			ScreenLog::setValue(1, to_string(int(1.f / clock.getElapsedTime().asSeconds())));
+			ScreenLog::setValue(0, to_string(int(1.f / clock.getElapsedTime().asSeconds())));
 			i = 0;
 		}
 		else
@@ -78,7 +78,7 @@ int main() {
 		clock.restart();
 		///////////////////////////////
 
-		ScreenLog::setValue(4, to_string(sf::Mouse::getPosition(window.canvas).x) + " | " + to_string(sf::Mouse::getPosition(window.canvas).y));
+		ScreenLog::setValue(3, to_string(sf::Mouse::getPosition(window.canvas).x) + " | " + to_string(sf::Mouse::getPosition(window.canvas).y));
 
 		World.Step(1 / 60.f, 8, 3);
 		window.updateState();
